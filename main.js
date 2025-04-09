429e38a5790c6b77796a17d144218052160f14f6
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-08 23:57:22 -0300

    criando as variaveis que vao receber as informacoes do calendario

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function colorirDia() {
+    // Pegar o valor de campo dia
+    let days = document.getElementById('day').value;
 
+    //pegar valor do campo cor
+    let color = document.getElementById('color').value;
+
+    //pegar os elementos da tabela calendário
+    let calendar = document.getElementById('calendar');
 }
