908efb6db41f579de61e7f5f5cfb9d64fea54ed5
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-09 00:02:23 -0300

    criando a condicao para verificar se o campo foi preenchido corretamente

--- main.js
+++ main.js
@@ -7,4 +7,15 @@ function colorirDia() {
 
     //pegar os elementos da tabela calendário
     let calendar = document.getElementById('calendar');
+
+    //verificar se o campo de dia foi preenchido
+    if (!days) {
+        alert("Favor informar o dia");
+    } else if ((days > 0) && (days <= 31)) {
+        // se o numero de dias está no untervalo aplicara a cor na célula do calendário
+        let td = calendar.getElementsByTagName('td')[parseInt(days) + 1];
+        td.style.backgroundColor = color;
+    } else {
+        alert("Favor informar um dia do calendário");
+    }
 }
